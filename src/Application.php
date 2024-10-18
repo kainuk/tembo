0c68fb4d24c7dda36c28283401ef6423728c5a45
Klaas Eikelboom <klaas.eikelboom@civicoop.org>
2024-10-18 13:13:54 +0200

    Load environment variables

--- src/Application.php
+++ src/Application.php
@@ -3,6 +3,7 @@
 namespace Tembo;
 
 use Symfony\Component\Console\Application as SymfonyApplication;
+use Symfony\Component\Dotenv\Dotenv;
 
 class Application extends SymfonyApplication{
 
@@ -11,6 +12,7 @@ class Application extends SymfonyApplication{
    */
   public function __construct($temboDir) {
     parent::__construct('Tembo - Common tools for development environments', 'v1.0');
+    (new Dotenv())->load($temboDir.'/.env');
     $this->temboDir = $temboDir;
     $this->add(new \Tembo\Command\CopyConfigCommand($temboDir));
     $this->add(new \Tembo\Command\ImportDatabaseCommand($temboDir));
